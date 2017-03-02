bff0383a4c6e8d3ecdeb419be22116fa30841343
louisscruz <louisstephancruz@me.com>
2017-03-01 22:07:46 -0800

    Build

--- dist/index.js
+++ dist/index.js
@@ -19,16 +19,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 _commander2.default.arguments('[entry] [scope]').option('-V --V', 'Return the current version').option('-l --login', 'Speficies that there should be an initial log in').option('-w --workers <n>', 'Specifies the number of workers', parseInt).parse(process.argv);
 
-// if (program.V) {
-//   (function printVersion() {
-//     var path = require('path');
-//     console.log(require(path.join(__dirname, '..', 'package.json')).version);
-//   }());
-//   process.exit();
-// }
-console.log(_commander2.default);
 if (_commander2.default.version) {
-  console.log('TRYING');
   console.log(require(_path2.default.join(__dirname, '..', 'package.json')).version);
 }
 
